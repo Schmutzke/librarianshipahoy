104919cffdcf2091777d8f294c659d605874a4ea
Schmutzke <101606057+Schmutzke@users.noreply.github.com>
2025-09-21 21:48:26 -0700

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://dieter-boolean.codeberg.page",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
