0a6da1d2864bed925f6170465eea198b06127e98
Schmutzke <101606057+Schmutzke@users.noreply.github.com>
2025-11-07 13:14:23 -0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "system-ui",
-        body: "system-ui",
+        header: "lato",
+        body: "lato",
         code: "IBM Plex Mono",
       },
       colors: {
